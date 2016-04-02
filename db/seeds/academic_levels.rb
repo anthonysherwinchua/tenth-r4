ffe26efe386bb6756000567ce83e68be75ce1509
Sherwin <anthonysherwinchua@gmail.com>
2016-04-02 11:05:56 +0800

    added more academic levels

--- db/seeds/academic_levels.rb
+++ db/seeds/academic_levels.rb
@@ -1,11 +1,15 @@
 [
-    {:id => 1, :name => "N/A"},
-    {:id => 2, :name => "Preschool"},
-    {:id => 3, :name => "Elementary school"},
-    {:id => 4, :name => "High school"},
-    {:id => 5, :name => "Tertiary education"},
-    {:id => 6, :name => "Vocational education"},
-    {:id => 7, :name => "Graduate education"}
+  {:id => 1, :name => "N/A"},
+  {:id => 2, :name => "Preschool"},
+  {:id => 3, :name => "Elementary school undergraduate"},
+  {:id => 4, :name => "High school undergraduate"},
+  {:id => 5, :name => "Tertiary education undergraduate"},
+  {:id => 6, :name => "Vocational education undergraduate"},
+  {:id => 7, :name => "Graduate education"},
+  {:id => 8, :name => "Elementary graduate"},
+  {:id => 9, :name => "High school graduate"},
+  {:id => 10, :name => "Tertiary education graduate"},
+  {:id => 11, :name => "Vocational education graduate"},
 ].each do |level|
   AcademicLevel.where(name: level[:name]).first_or_create
 end
