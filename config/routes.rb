ac2c35d41482ace127e46980401e23ba2a8ef8a1
Sherwin <anthonysherwinchua@gmail.com>
2016-04-22 22:20:13 +0800

    fixed admin/applicants routing spec; added back show route

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     get '/dashboard', to: 'base#dashboard'
 
     resources :academic_levels
-    resources :applicants, only: [:index, :new, :create, :edit, :update]
+    resources :applicants, only: [:index, :show, :new, :create, :edit, :update]
     resources :civil_statuses
     resources :contact_types
     resources :countries
