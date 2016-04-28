7ddb6600003fa6796bf391dc12b7f646d03d2c1c
Sherwin <anthonysherwinchua@gmail.com>
2016-04-29 00:29:51 +0800

    experiment of auto-layouting the attributes for the form

--- app/forms/applicant_wizard_form/steps/personal_info_step.rb
+++ app/forms/applicant_wizard_form/steps/personal_info_step.rb
@@ -1,6 +1,7 @@
 class ApplicantWizardForm::Steps::PersonalInfoStep < WizardForm::Step
 
-  attr_accessor :first_name, :last_name
+  attr_accessor :first_name, :last_name, :middle_name, :suffix_name, :birthday, :birthplace, :gender, :height, :weight,
+                :video_url, :religion, :civil_status
 
   validates :first_name, :last_name, presence: true
 
