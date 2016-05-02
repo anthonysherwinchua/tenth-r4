70355562fd9ec45f213b65a92eb302ebef5c2df9
Sherwin <anthonysherwinchua@gmail.com>
2016-05-02 21:14:32 +0800

    Finished personal info step as an example for other steps

--- app/forms/applicant_wizard_form/steps/personal_info_step.rb
+++ app/forms/applicant_wizard_form/steps/personal_info_step.rb
@@ -1,8 +1,45 @@
 class ApplicantWizardForm::Steps::PersonalInfoStep < WizardForm::Step
 
-  attr_accessor :first_name, :last_name, :middle_name, :suffix_name, :birthday, :birthplace, :gender, :height, :weight,
-                :video_url, :religion, :civil_status
+  main_model :applicant, Applicant
 
-  validates :first_name, :last_name, presence: true
+  has_one :father, ApplicantFamilyMember
+  has_one :mother, ApplicantFamilyMember
+  has_one :spouse, ApplicantFamilyMember
+
+  def initialize(applicant)
+    @applicant = applicant
+    prepare_relationships(:father, :mother, :spouse)
+  end
+
+  def save(params)
+    Applicant.transaction do
+      @father.attributes = params.delete 'father'
+      @mother.attributes = params.delete 'mother'
+      @spouse.attributes = params.delete 'spouse'
+      @applicant.attributes = params
+      @applicant.save!
+      @father.update_attributes!(applicant: @applicant)
+      @mother.update_attributes!(applicant: @applicant)
+      @spouse.update_attributes!(applicant: @applicant)
+    end
+  rescue ActiveRecord::RecordInvalid => e
+    @father.valid?
+    @mother.valid?
+    @spouse.valid?
+    false
+  end
+
+  private
+
+  def prepare_relationships(*names)
+    names.each{|name| prepare_relationship name}
+  end
+
+  def prepare_relationship(name)
+    relationship = Relationship.where(["lower(name) = ?", name]).first
+    family_member = ApplicantFamilyMember.where(relationship_id: relationship.id)
+                      .first_or_initialize
+    instance_variable_set("@#{name}", family_member)
+  end
 
 end
