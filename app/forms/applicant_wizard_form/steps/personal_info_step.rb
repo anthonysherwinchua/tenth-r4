acf8dc970d8ec10c4c59cccd98588bb86ec29c58
Sherwin <anthonysherwinchua@gmail.com>
2016-05-11 11:12:26 +0800

    check spouse only when applicant is not single or n/a

--- app/forms/applicant_wizard_form/steps/personal_info_step.rb
+++ app/forms/applicant_wizard_form/steps/personal_info_step.rb
@@ -20,13 +20,13 @@ class ApplicantWizardForm::Steps::PersonalInfoStep < WizardForm::Step
       @applicant.save!
       @father.update_attributes!(applicant: @applicant)
       @mother.update_attributes!(applicant: @applicant)
-      @spouse.update_attributes!(applicant: @applicant)
+      @spouse.update_attributes!(applicant: @applicant) if spouse_required?
     end
     true
   rescue ActiveRecord::RecordInvalid => e
     @father.valid?
     @mother.valid?
-    @spouse.valid?
+    @spouse.valid? if spouse_required?
     false
   end
 
@@ -38,12 +38,18 @@ class ApplicantWizardForm::Steps::PersonalInfoStep < WizardForm::Step
 
   def prepare_relationship(name)
     relationship = Relationship.where(["lower(name) = ?", name]).first
-    family_member = if @applicant.persisted?
-                      @applicant.applicant_family_members.find_by(relationship_id: relationship.id)
-                    else
-                      ApplicantFamilyMember.new(relationship_id: relationship.id)
-                    end
+    if @applicant.persisted?
+      family_member = @applicant.applicant_family_members.find_by(relationship_id: relationship.id)
+    end
+
+    unless family_member
+      family_member = ApplicantFamilyMember.new(relationship_id: relationship.id)
+    end
     instance_variable_set("@#{name}", family_member)
+  end
+
+  def spouse_required?
+    ['n/a', 'single'].exclude? @applicant.civil_status.name.downcase
   end
 
 end
