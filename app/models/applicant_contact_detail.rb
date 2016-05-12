43cda2e5b1bea0abb54dc513b66020a032901a81
Sherwin <anthonysherwinchua@gmail.com>
2016-05-12 23:00:14 +0800

    added applicant contact details

---
+++ app/models/applicant_contact_detail.rb
@@ -0,0 +1,9 @@
+﻿class ApplicantContactDetail < ActiveRecord::Base
+
+  belongs_to :applicant, inverse_of: :applicant_contact_details
+  belongs_to :contact_type, inverse_of: :applicant_contact_details
+
+  validates :value, presence: true
+  validates :applicant, :contact_type, presence: true
+
+end
--- app/models/contact_type.rb
+++ app/models/contact_type.rb
@@ -1,5 +1,7 @@
 class ContactType < ActiveRecord::Base
 
+  has_many :applicant_contact_details, inverse_of: :contact_type
+
   validates :name, presence: true, uniqueness: { case_sensitive: false }
 
 end
